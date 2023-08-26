5bbb31f59bddc928369ab14cbbccdaef9d8c78ea
김모남 <138136517+monam2@users.noreply.github.com>
2023-08-26 23:10:44 +0900

    Update prgs 1844.py

--- algorithm/BFS/prgs 1844.py
+++ algorithm/BFS/prgs 1844.py
@@ -4,6 +4,7 @@ from collections import deque
 def BFS(x,y,maps):
     dx = [-1,0,1,0]
     dy = [0,1,0,-1]
+    visited = [[False]*len(maps[0]) for _ in range(len(maps))]
     q=deque()
     q.append((x,y))
 
@@ -14,15 +15,15 @@ def BFS(x,y,maps):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx >=0 and ny >= 0 and nx < len(maps) and ny < len(maps[0]) and maps[nx][ny] != 0:
-
+            if nx >= 0 and ny >= 0 and nx < len(maps) and ny < len(maps[0]):
+                if visited[nx][ny] == False and maps[nx][ny] !=0:
+                    q.append((nx,ny))
+                    visited[nx][ny]=True
+                    maps[nx][ny] = maps[x][y]+1
+    return -1
 
 def solution(maps):
-    answer = -1
-    if BFS(0,0,maps):
-        return BFS(0,0,maps)
-    else:
-        return -1
+    return BFS(0,0,maps)
 
 print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
 print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]))
