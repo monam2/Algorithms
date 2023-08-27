e4771ee3a0366ad35e5addec735844c7b776eb24
김모남 <138136517+monam2@users.noreply.github.com>
2023-08-27 19:36:56 +0900

    Update boj 1260.py

--- algorithm/DFS/boj 1260.py
+++ algorithm/DFS/boj 1260.py
@@ -2,23 +2,56 @@
 
 from collections import deque
 def DFS(graph, v):
+    for i in graph:
+        i.sort(reverse=True)
     stack = []
+    result = []
     visited = [False] * (n+1)
     stack.append(v)
-    visited[v] = True
-
     while stack:
         v = stack.pop()
-        
+        if visited[v] == False:
+            visited[v] = True
+            result.append(v)
+        else:
+            continue
+        for i in range(len(graph[v])):
+            if visited[graph[v][i]] == False:
+                stack.append(graph[v][i])
 
+    return result
 
 def BFS(graph, v):
+    for i in graph:
+        i.sort()
     q=deque()
+    result = []
+    visited = [False] * (n+1)
+    q.append(v)
+    while q:
+        v = q.popleft()
+        if visited[v] == False:
+            visited[v]=True
+            result.append(v)
+        else:
+            continue
+        for i in range(len(graph[v])):
+            if visited[graph[v][i]] == False:
+                q.append(graph[v][i])
+
+    return result
+
 n,m,k = map(int, input().split())
 graph = [[] for _ in range(n+1)]
 for i in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-DFS(graph, k)
-BFS(graph, k)
+
+dfs_result = DFS(graph, k)
+bfs_result = BFS(graph, k)
+for j in dfs_result:
+    print(j, end=' ')
+print()
+for j in bfs_result:
+    print(j, end=' ')
