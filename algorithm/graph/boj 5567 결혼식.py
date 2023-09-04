9761190aac3015bd2dc0582d929c3e6d9b749d30
김모남 <138136517+monam2@users.noreply.github.com>
2023-09-04 21:22:52 +0900

    Create boj 5567 결혼식.py

---
+++ algorithm/graph/boj 5567 결혼식.py
@@ -0,0 +1,33 @@
+#5567 결혼식
+from collections import deque
+
+def DFS(v,graph,visited):
+    q = deque()
+    visited[v]=True
+    q.append(v)
+    count = 0
+    result = 0
+
+    while q:
+        count += 1
+        for _ in range(len(q)):
+            v = q.popleft()
+            for i in range(len(graph[v])):
+                if visited[graph[v][i]]==False:
+                    visited[graph[v][i]] = True
+                    result += 1
+                    q.append(graph[v][i])
+        if count == 2:
+            break
+    return result
+
+n=int(input())
+m=int(input())
+graph = [[] for _ in range(n+1)]
+for i in range(m):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+visited =[False]*(n+1)
+print(DFS(1,graph,visited))
