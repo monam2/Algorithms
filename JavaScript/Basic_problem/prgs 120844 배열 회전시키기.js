f284f042b07ecdea6afef42cb8dfe9626659016b
모남 <138136517+monam2@users.noreply.github.com>
2023-10-08 23:43:28 +0900

    Update prgs 120844 배열 회전시키기.js

--- JavaScript/Basic_problem/prgs 120844 배열 회전시키기.js
+++ JavaScript/Basic_problem/prgs 120844 배열 회전시키기.js
@@ -1,11 +1,16 @@
 //프로그래머스 120844 배열 회전시키기
 function solution(numbers, direction) {
-    if (direction==='right'){
-        let temp = numbers.pop();
-        numbers.unshift(temp);
-    } else {
-        let temp = numbers.shift();
-        numbers.push(temp);
-    }
-    return numbers;
-}
+    return direction==='right' ?  [numbers.pop(), ...numbers] : [...numbers.slice(1), numbers.shift()]
+} 
+// ... : 배열 전개해서 새로운 배열 생성
+
+// function solution(numbers, direction) {
+//     if (direction==='right'){
+//         let temp = numbers.pop();
+//         numbers.unshift(temp);
+//     } else {
+//         let temp = numbers.shift();
+//         numbers.push(temp);
+//     }
+//     return numbers;
+// }
