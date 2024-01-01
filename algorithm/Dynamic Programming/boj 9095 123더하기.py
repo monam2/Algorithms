7b68af04fc765be1d3936b19b9fe504f1d19d20a
monam2 <kangcw0107@gmail.com>
2024-01-01 22:59:29 +0900

    DP

---
+++ algorithm/Dynamic Programming/boj 9095 123더하기.py
@@ -0,0 +1,15 @@
+#백준 9095 1,2,3더하기
+
+t = int(input())
+for _ in range(t):
+    dp = [0]*11
+    dp[0]=0
+    dp[1]=1
+    dp[2]=2
+    dp[3]=4
+
+    n = int(input())
+
+    for i in range(4,n+1):
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    print(dp[n])
