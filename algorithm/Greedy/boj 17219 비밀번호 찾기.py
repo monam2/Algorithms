b19d8079c9570a58843bc048263449463dfac86a
monam2 <kangcw0107@gmail.com>
2024-01-14 17:27:04 +0900

    re

--- algorithm/Greedy/boj 17219 비밀번호 찾기.py
+++ algorithm/Greedy/boj 17219 비밀번호 찾기.py
@@ -1,4 +1,6 @@
 #boj 17219 비밀번호 찾기
+import sys
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
@@ -6,6 +8,7 @@ info = {}
 for i in range(n):
     url, pw = map(str, input().split())
     info[url] = pw
+    
 for j in range(m):
     what = input()
     print(info[what])
