1fd86fade297feb1dec6a86ad0851ead876f3b8d
강창우(monam2) <138136517+monam2@users.noreply.github.com>
2024-02-20 10:35:06 +0900

    Update Main_2467.java

--- algorithm/two pointer/Main_2467.java
+++ algorithm/two pointer/Main_2467.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class sdaf {
+public class Main_2467 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st = null;
 	
