964159b78c73a47de1636865b60263e552d05969
강창우(monam2) <138136517+monam2@users.noreply.github.com>
2024-03-29 02:04:51 +0900

    백준 11053 가장 긴 증가하는 부분 수열

---
+++ js/BOJ11053.js
@@ -0,0 +1,26 @@
+// 백준 11053 가장 긴 증가하는 부분 수열
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+
+let [n, str] = input;
+n = +n;
+const arr = str.split(" ").map(i=>+i);
+let dp = new Array(n).fill(1);
+
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+        if (arr[j] < arr[i]) {
+            dp[i] = Math.max(dp[i], dp[j]+1)
+        }
+    }
+}
+
+let max = 0;
+dp.forEach(i=>{
+    if (i>max) {
+        max = i;
+    }
+})
+console.log(max);
